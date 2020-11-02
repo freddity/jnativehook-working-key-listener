a0551508c0af3bbd8f0646c5f7de2da1b42a89bd
Ivan Iwanow <kubaice3@gmail.com>
2020-11-02 11:14:14 +0100

    initial commit

---
+++ src/main/java/GlobalKeyListenerExample.java
@@ -0,0 +1,52 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class GlobalKeyListenerExample implements NativeKeyListener {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+        //line 15 check which key was clicked
+        if (e.getKeyCode() == NativeKeyEvent.VC_ENTER) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException nativeHookException) {
+                nativeHookException.printStackTrace();
+            }
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
+
+    public static void main(String[] args) {
+
+        // Clear previous logging configurations.
+        LogManager.getLogManager().reset();
+
+        // Get the logger for "org.jnativehook" and set the level to off.
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListenerExample());
+    }
+}
